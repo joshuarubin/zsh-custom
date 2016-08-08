b3e35c19dd67bf58408167aaf693837e153e1d5a
Joshua Rubin <jrubin@zvelo.com>
2016-08-08 17:05:46 -0600

    add support for cuda

--- custom.plugin.zsh
+++ custom.plugin.zsh
@@ -14,6 +14,12 @@ else
   path[1,0]="$HOME/working/android-sdk-linux/platform-tools"
 fi
 
+if [ -d /usr/local/cuda ]; then
+  export CUDA_HOME=/usr/local/cuda
+  export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$CUDA_HOME/lib"
+  path[1,0]="$CUDA_HOME/bin"
+fi
+
 path[1,0]="/usr/local/google-cloud-sdk/bin"
 path[1,0]="$HOME/.cabal/bin"
 path[1,0]="$HOME/.cargo/bin"
